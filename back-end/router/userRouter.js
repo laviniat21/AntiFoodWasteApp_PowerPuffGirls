f8547b165324cf00874286dc9e1f2a40f7b2f0b2
laviniat21 <tudorlavinia21@gmail.com>
2023-01-16 06:45:29 +0200

    deleted files

--- back-end/router/userRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { registerUser, loginUser } from "../controller/userController"
-const router = express.Router();
-
-router.route("/").get();
-// router.route("")
-
-export default router;
